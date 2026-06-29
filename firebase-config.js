217c9cb39da327d4fe8848a38678b5a6c12b7a19
mostafa dahrog <111236159+mosfa-cod@users.noreply.github.com>
2026-06-29 16:40:55 +0100

    Reformat firebase-config.js with consistent imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-// ===== firebase-config.js =====
+ // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";

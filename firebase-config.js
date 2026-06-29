a73ab71840f1e66fa8a705d8fd57e536a4a77f1f
mostafa dahrog <111236159+mosfa-cod@users.noreply.github.com>
2026-06-29 20:10:04 +0100

    Refactor Firebase config for classic setup
    Updated Firebase configuration to use the classic setup and added database URL.

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
- // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-
+ // إعدادات الفايربيس الكلاسيكية المتوافقة مع الموقع ولوحة التحكم
 const firebaseConfig = {
-  apiKey: "AIzaSyBMvIzV131KdAjuHS8B6tCUBsmaGgloLUY",
-  authDomain: "mostafa-acdemy-2645a.firebaseapp.com",
-  projectId: "mostafa-acdemy-2645a",
-  storageBucket: "mostafa-acdemy-2645a.firebasestorage.app",
-  messagingSenderId: "371101699494",
-  appId: "1:371101699494:web:3e79c870941afef5179367"
+    apiKey: "AIzaSyBMvIZv131KdAjuH58B6tCUBSmaGgLoLUY",
+    authDomain: "://firebaseapp.com",
+    databaseURL: "https://firebaseio.com",
+    projectId: "mostafa-academy-2645a",
+    storageBucket: "://appspot.com",
+    messagingSenderId: "371101699494",
+    appId: "1:371101699494:web:3e79c870941afef5179367"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// تهيئة الفايربيس
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+// تعريف المتغيرات للاستخدام في ملف main.js
+const db = firebase.database();
+const auth = firebase.auth();
